551acb1ec35b7d774232891d7c3d983ee4ff77fc
ibykus87 <92509939+ibykus87@users.noreply.github.com>
2024-10-16 19:33:24 +0200

    Create get human choice function
    Step 3: Write the logic to get the human choice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Randomly generate one of the following string values: "rock", "paper" or "scissors" with a function
 function getComputerChoice () {
-// 1. Initialise variable 
+// 1. Initialize computer choice variable 
     let computerChoice = ''; 
 // 2. Get a random number
     randomNumber = Math.random();
@@ -20,4 +20,21 @@ function getComputerChoice () {
     return( computerChoice );
 };
 
-console.log(getComputerChoice());
+// Step 3: Write the logic to get the human choice
+
+// Write a function that takes the user choice and returns it
+function getHumanChoice () {
+// 1. Initialize user input variable and ask the user for his choice 
+    let userInput = prompt('Enter "rock", "paper" or "scissors" to play the game:');
+// 2 Convert user input to lower case
+    let lowerCaseUserInput = userInput.toLowerCase();
+// 3. Check if human input is a valid choice, return it or let the user know that the input is not valid
+    if ( lowerCaseUserInput === 'rock' || lowerCaseUserInput === 'paper' || lowerCaseUserInput === 'scissors') {
+        return( lowerCaseUserInput );
+    }
+    else {
+        return( console.log( 'Your choice is not valid. Please enter "rock", "paper" or "scissors" into the console' ));
+    }
+};
+
+getHumanChoice();
